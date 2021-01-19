760d207575afe2315a8eb4befa5af3d889901a8b
liuxiaodong <1119441533@qq.com>
2021-01-19 15:10:39 +0800

    更新测试文件

--- test/test.js
+++ test/test.js
@@ -1,4 +1,32 @@
 let {
-	generateUUID
-} = require('./util');
-console.lgo(generateUUID());
+  generateUUID
+} = require('../build/util.min.js');
+console.log(generateUUID());
+
+let {
+  toSafeQueryString,
+  parseSafeQueryString,
+} = require("../build/querystring.min.js");
+
+let jsonObj = {
+  a: 1,
+  b: 'false',
+  c: true,
+  d: null,
+  e: undefined,
+  f: [1, 23, 4, 5, 6, 78],
+  g: {
+    attr: 'attr',
+    func: function() {
+      console.log('Hello')
+    }
+  },
+  h: '11222',
+  i: 'null',
+  j: 'undefined',
+}
+console.log(jsonObj);
+jsonObj = toSafeQueryString(jsonObj);
+console.log(jsonObj);
+jsonObj = parseSafeQueryString(jsonObj);
+console.log(jsonObj);
