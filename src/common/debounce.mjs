c8aad46d9caa7c92036c0061604e6526de15ed90
liuxiaodong <1119441533@qq.com>
2021-02-03 10:36:32 +0800

    添加防抖节流工具

---
+++ src/common/debounce.mjs
@@ -0,0 +1,42 @@
+(function() {
+
+  // 防抖：触发操作后。并间隔delay时长，才执行函数，如果在delay时长内又触发了操作，则重新调整函数执行时间
+  const debounce = function(fn, delay = 50, immediate = false) {
+    var timer;
+    return function(...rest) {
+      if (timer) clearTimeout(timer);
+      // 是否立即执行
+      if (immediate) {
+        var isCalledNow = !timer;
+        timer = setTimeout(function() {
+          timer = null;
+        }, delay)
+        if (isCalledNow) fn.call(this, ...rest);
+      } else {
+        timer = setTimeout(() => {
+          // 表明fn不能是箭头函数
+          fn.call(this, ...rest);
+          timer = null;
+        }, delay)
+      }
+    }
+  }
+
+  if (typeof define === 'function') {
+    define(function() {
+      return debounce;
+    });
+  }
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = debounce;
+  }
+
+  function isInWindow() {
+    return 'undefined' !== typeof window && /^\[object (?:Window|DOMWindow|global)\]$/.test(toString.call(window));
+  }
+  if (isInWindow()) {
+    window.debounce = debounce;
+  }
+
+})()
---
+++ src/common/throttle.mjs
@@ -0,0 +1,34 @@
+(function() {
+
+  // 节流：在指定间隔时间内只触发一次，无论期间触发操作多少次
+  const throttle = function(fn, delay = 50) {
+    var timer;
+    return function(...rest) {
+      if (!timer) {
+        timer = setTimeout(function() {
+          // 表明fn不能是箭头函数
+          fn.call(this, ...rest);
+          timer = null;
+        }, delay);
+      }
+    }
+  }
+
+  if (typeof define === 'function') {
+    define(function() {
+      return throttle;
+    });
+  }
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = throttle;
+  }
+
+  function isInWindow() {
+    return 'undefined' !== typeof window && /^\[object (?:Window|DOMWindow|global)\]$/.test(toString.call(window));
+  }
+  if (isInWindow()) {
+    window.throttle = throttle;
+  }
+
+})()
