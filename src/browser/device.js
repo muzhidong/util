9c44c1c636b4702323f48583d85708340bb70ea1
muzhidong <be_your_best@163.com>
2026-01-06 11:41:34 +0800

    feat: 设备检查工具更新

--- src/browser/device.js
+++ src/browser/device.js
@@ -2,51 +2,44 @@
 
   if (!(/^\[object (?:Window|DOMWindow|global)\]$/.test(toString.call(context)))) return;
 
-  var WIN = window;
-  var LOC = WIN["location"];
-  var NA = WIN.navigator;
-  var UA = NA.userAgent.toLowerCase();
+  const WIN = window;
+  const NA = WIN.navigator;
+  const UA = NA.userAgent.toLowerCase();
 
   function test(needle) {
     return needle.test(UA);
   }
 
-  var IsTouch = "ontouchend" in WIN;
-  var IsAndroid = test(/android|htc/) || /linux/i.test(NA.platform + "");
-  var IsIPad = !IsAndroid && test(/ipad/);
-  var IsIPhone = !IsAndroid && test(/ipod|iphone/);
-  var IsIOS = IsIPad || IsIPhone;
-  var IsWinPhone = test(/windows phone/);
-  var IsWebapp = !!NA["standalone"];
-  var IsXiaoMi = IsAndroid && test(/mi\s+/);
-  var IsUC = test(/ucbrowser/);
-  var IsWeixin = test(/micromessenger/);
-  var IsBaiduBrowser = test(/baidubrowser/);
-  var IsChrome = !!WIN["chrome"];
-  var IsBaiduBox = test(/baiduboxapp/);
-  var IsPC = !IsAndroid && !IsIOS && !IsWinPhone;
-  var IsHTC = IsAndroid && test(/htc\s+/);
-  var IsBaiduWallet = test(/baiduwallet/);
-  var IsDebug = !!~("" + LOC["port"]).indexOf("0");
+  const isAndroid = test(/android/) || /linux/i.test(NA.platform + "");
+  const isIPad = !isAndroid && test(/ipad/);
+  const isiPhone = !isAndroid && test(/ipod|iphone/);
+  const isiOS = isiPhone || isiPad;
 
-  var device = {
-    isTouch: IsTouch,
-    isAndroid: IsAndroid,
-    isIPad: IsIPad,
-    isIPhone: IsIPhone,
-    isIOS: IsIOS,
-    isWinPhone: IsWinPhone,
-    isWebapp: IsWebapp,
-    isXiaoMi: IsXiaoMi,
-    isUC: IsUC,
-    isWeixin: IsWeixin,
-    isBaiduBox: IsBaiduBox,
-    isBaiduBrowser: IsBaiduBrowser,
-    isChrome: IsChrome,
-    isPC: IsPC,
-    isHTC: IsHTC,
-    isBaiduWallet: IsBaiduWallet,
-    isDebug: IsDebug
+  const isMobile = test(/applewebkit.*mobile.*/);
+  const isPC = !isAndroid && !isiOS;
+
+  const isTouch = "ontouchend" in WIN;
+
+  const isWeixin = test(/micromessenger/);
+  const isXiaoMi = isAndroid && test(/mi\s+/);
+  const isUC = test(/ucbrowser/);
+  const isChrome = !!WIN["chrome"];
+
+  const device = {
+    isAndroid,
+    isIPad,
+    isIPhone,
+    isIOS,
+
+    isPC,
+    isMobile,
+
+    isTouch,
+
+    isWeixin,
+    isXiaoMi,
+    isUC,
+    isChrome,
   };
 
   context.util = context.util || {}
